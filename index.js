f2e2ed4d8842a65f410c8d5ed10aac73696db93c
mahardika <mahardicka404@gmail.com>
2019-09-17 11:32:09 +0700

    resume

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import codePush from 'react-native-code-push'
 import App from './App'
 import { name as appName } from './app.json'
 
-const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.ON_NEXT_SUSPEND }
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME, installMode: codePush.InstallMode.ON_NEXT_RESUME }
 const PushApp = codePush(codePushOptions)(App)
 
 AppRegistry.registerComponent(appName, () => PushApp)
